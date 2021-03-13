a2d724517a1b7f29da8c4eff2813bca5e8c4e271
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-03-13 11:24:40 +0900

    add file

---
+++ ChatCore/Models/BiliBili/BiliBiliChatChannel.cs
@@ -0,0 +1,26 @@
+using ChatCore.Interfaces;
+using ChatCore.Utilities;
+
+namespace ChatCore.Models.BiliBili
+{
+    public class BiliBiliChatChannel : IChatChannel
+    {
+        public string Id { get; internal set; } = null!;
+        public string Name { get; internal set; } = null!;
+
+        public BiliBiliChatChannel() { }
+        public BiliBiliChatChannel(string json)
+        {
+            var obj = JSON.Parse(json);
+            if (obj.TryGetKey(nameof(Id), out var id)) { Id = id.Value; }
+            if (obj.TryGetKey(nameof(Name), out var name)) { Name = name.Value; }
+        }
+        public JSONObject ToJson()
+        {
+            var obj = new JSONObject();
+            obj.Add(nameof(Id), new JSONString(Id));
+            obj.Add(nameof(Name), new JSONString(Name));
+            return obj;
+        }
+    }
+}
